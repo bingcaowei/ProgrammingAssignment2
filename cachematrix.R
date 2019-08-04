55fb2ed9bb888cc551371b0fbb2f39df481e1a25
Annie <sweetapple_281@hotmail.com>
2019-08-04 12:54:12 -0400

    completed hw3

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## bingcaowei August 2019
+## https://github.com/bingcaowei/ProgrammingAssignment2
+## HW3 for R Programming Coursera
 
-## Write a short comment describing this function
-
+## Creates a special matrix object that can cache its inverse
+## assume matrix is always invertible
 makeCacheMatrix <- function(x = matrix()) {
+        
+        inv <- NULL
+        set <- function (y){
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(inverse) inv <<- inverse
+        getInverse <- function() inv
+        
+        list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 
 }
 
 
-## Write a short comment describing this function
-
+## computes the inverse of the special matrix returned by 
+## makeCacheMatrix. If inverse has already been calculated 
+## (and matrix has not changed), then cacheSolve should retrieve
+## inverse from cache
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
+        inv <- x$getInverse()
+        mat <- x$get()
+        if(!is.null(inv)){
+                message("getting cached data")
+                return (inv)
+        }
+        inv <- solve(mat,...)
+        x$setInverse(inv)
+        inv
 }
